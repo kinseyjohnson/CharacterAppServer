c72f1d2a2e5bead1a58a857b6580af1908ceb4b9
Freya Black <freyablackdev@gmail.com>
2021-07-28 17:18:02 -0400

    /findall & /delete/:id endpoints created

--- controllers/charactercontroller.js
+++ controllers/charactercontroller.js
@@ -28,7 +28,37 @@ router.post('/create', /*validation,*/ async (req, res) => {
     } catch (err) {
         res.status(500).json({error: err});
     }
-    CharacterModel.create(characterEntry)
+    // CharacterModel.create(characterEntry)
+})
+
+router.get('/findAll', async (req, res) => {
+    try {
+        const allCharacters = await CharacterModel.findAll();
+        res.status(200).json(allCharacters);
+    } catch (err) {
+        res.status(500).json({
+            error: err,
+        })
+    }
+})
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        await CharacterModel.destroy({
+            where: {
+                id: req.params.id
+            },
+        }).then((result) => {
+            res.status(200).json({
+                message: 'Character sucessfully retired',
+                deletedCharacter: result,
+            });
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: `Failed to retire character: ${err}`
+        })
+    }
 })
 
 module.exports = router;
