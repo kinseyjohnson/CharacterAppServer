a6de128a88e6aee80a9a19b5da27dd983c54893a
Freya Black <freyablackdev@gmail.com>
2021-08-03 14:13:00 -0400

    deploy

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.db)
+const sequelize = new Sequelize(process.env.db, {
+    dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT === 'development',
+});
 
 module.exports = sequelize;
 
