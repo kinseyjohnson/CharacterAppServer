8fe578a5a689cbbea8adbffd5d559f033c1629cf
Freya Black <freyablackdev@gmail.com>
2021-08-03 15:05:56 -0400

    heroku deploy

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.db)
+const sequelize = new Sequelize(process.env.db, {
+    dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT === 'development',
+});
 
 module.exports = sequelize;
-
-
