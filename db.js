62a72098c8b1f07d5e12fa5f9031a970d14e28d6
Freya Black <freyablackdev@gmail.com>
2021-08-04 11:00:03 -0400

    fix merge conflicts

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.db, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'development',
 });
