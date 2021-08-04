61d2d640fabea8e0e8fed32510d4d3af33953e36
Freya Black <freyablackdev@gmail.com>
2021-08-04 13:54:06 -0400

    added /usercharacters route

--- controllers/charactercontroller.js
+++ controllers/charactercontroller.js
@@ -52,24 +52,23 @@ router.get('/findAll', validateJWT, async (req, res) => {
     }
 })
 
-// router.delete('/delete/:id', validateJWT, async (req, res) => {
-//     try {
-//         await CharacterModel.destroy({
-//             where: {
-//                 id: req.params.id
-//             },
-//         }).then((result) => {
-//             res.status(200).json({
-//                 message: 'Character sucessfully retired',
-//                 deletedCharacter: result,
-//             });
-//         });
-//     } catch (err) {
-//         res.status(500).json({
-//             message: `Failed to retire character: ${err}`
-//         })
-//     }
-// })
+router.get('/usercharacters', validateJWT, async (req, res) => {
+    const username = req.user.username
+    try {
+        const query = {
+            where: {
+                owner: username
+            }
+        };
+        
+        const userCharacters = await CharacterModel.findAll(query);
+        res.status(200).json(userCharacters);
+    } catch (err) {
+        res.status(500).json({
+            error: err
+        })
+    }
+})
 
 router.delete('/delete/:id', validateJWT, async (req, res) => {
     const username = req.user.username;
