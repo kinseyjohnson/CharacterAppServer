1f4f5f55e795e0676442ff4d0c06869c3867009b
Freya Black <freyablackdev@gmail.com>
2021-08-04 12:45:59 -0400

    few fixes

--- db.js
+++ db.js
@@ -1,24 +1,8 @@
-// const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 185c7a73d9b0fc39be6aba8e196da1a4f6508f89
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'development',
 });
 
-<<<<<<< HEAD
-=======
-
-
-// module.exports = sequelize;
-
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("postgres://postgres:hellokinsey@localhost:5432/workout-log-server");
-
->>>>>>> 185c7a73d9b0fc39be6aba8e196da1a4f6508f89
 module.exports = sequelize;
