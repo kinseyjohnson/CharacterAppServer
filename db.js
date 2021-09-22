e6fe0e15cd2b11441b650915271590f248b8b1da
Freya Black <freyablackdev@gmail.com>
2021-09-22 17:01:53 -0400

    Aug 22nd commit

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    ssl: process.env.ENVIRONMENT === 'development',
+    ssl: process.env.ENVIRONMENT === 'production',
 });
 
 module.exports = sequelize;
